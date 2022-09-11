c98d5a4bfd2d123e7700c9620b4f9520eafff65e
Kian <mr.kian.vf@gmail.com>
2022-09-11 15:06:50 +0430

    Update twBot.py

--- twBot.py
+++ twBot.py
@@ -1,13 +1,15 @@
 import selenium
 from selenium import webdriver
-# path to web driver
-driverPath = r'C:\Users\program\Desktop\kPy\WebDriver\geckodriver.exe' 
-# URL to open space
-spaceUrl = 'https://twitter.com/i/spaces/1LyxBqMEMeEJN'
-browser = webdriver.Firefox(
-    executable_path=driverPath)
-for _ in range(10):
-    browser.get(spaceUrl)
-    # finding the join button
-    btn = browser.find_element_by_css_selector("div[role = 'button']")
-    btn.click()
+driverPath = input(
+    "Enter valid geckodriver.exe path example: C:\\Users\\program\\Desktop\\kPy\\WebDriver\\geckodriver.exe: ")
+spaceUrl = input(
+    "Enter a valid active twitter space URl Example : https://twitter.com/i/spaces/1LyxBqMEMeEJN: ")
+n = int(input("Enter how many times you wish to connect to the space: "))
+browser = webdriver.Firefox(executable_path=driverPath)
+for _ in range(n):
+    try:
+        browser.get(spaceUrl)
+        btn = browser.find_element_by_css_selector("div[role = 'button']")
+        btn.click()
+    except:
+        print("Error occurred")
